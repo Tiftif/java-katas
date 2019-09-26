9ef45de7c956266dbaa2147162c2a845062ac00e
Tiftif <gab.berteloot@gmail.com>
2019-09-26 10:13:10 +0200

    FindMin

--- src/kata/FoundMin.java
+++ src/kata/FoundMin.java
@@ -1,4 +1,15 @@
 public class FoundMin {
 
     // TODO
+  public int findMinInArray(int[] intArray){
+    int min = intArray[0];
+
+    for(int i = 1; i < intArray.length; i++){
+      if(min > intArray[i]){
+        min = intArray[i];
+      }
+    }
+
+    return min;
+  }
 }
--- src/test/FoundMinTest.java
+++ src/test/FoundMinTest.java
@@ -5,7 +5,9 @@ public class FoundMinTest {
 
     @Test
     public void testTrue() throws Exception {
-        Assert.assertEquals(true, true);
+        FoundMin myFoundMin = new FoundMin();
+        Assert.assertEquals(1, myFoundMin.findMinInArray(new int[]{5, 8, 9, 2, 1}));
+        Assert.assertEquals(-4, myFoundMin.findMinInArray(new int[]{5, 8, 9, -4, 1}));
     }
 
     // TODO

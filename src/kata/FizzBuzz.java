c143d41a9b518d776419abd65d5a5238101ebda4
Tiftif <gab.berteloot@gmail.com>
2019-09-26 09:58:25 +0200

    solution Fizzbuzz

--- src/kata/FizzBuzz.java
+++ src/kata/FizzBuzz.java
@@ -1,4 +1,16 @@
 public class FizzBuzz {
 
-    // TODO
+    public String getWord(int multiple){
+      String result = null;
+      if (multiple % 3 == 0 && multiple % 5 == 0){
+        result = "FizzBuzz";
+      } else if(multiple % 5 == 0){
+        result = "Buzz";
+      } else if (multiple % 3 == 0) {
+        result = "Fizz";
+      } else{
+        result = String.valueOf(multiple);
+      }
+      return result;
+    }
 }
--- src/test/FizzBuzzTest.java
+++ src/test/FizzBuzzTest.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 public class FizzBuzzTest {
 
     @Test
-    public void testTrue() throws Exception {
-        Assert.assertEquals(true, true);
+    public void testFizzBuzz() throws Exception {
+        FizzBuzz myFizzBuzz = new FizzBuzz();
+        Assert.assertEquals("Fizz", myFizzBuzz.getWord(3));
+        Assert.assertEquals("Buzz", myFizzBuzz.getWord(5));
+        Assert.assertEquals("FizzBuzz", myFizzBuzz.getWord(15));
+        Assert.assertEquals("11", myFizzBuzz.getWord(11));
+        Assert.assertEquals("17", myFizzBuzz.getWord(17));
     }
 
-    // TODO
 }
